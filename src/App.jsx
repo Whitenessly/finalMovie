686904fa6c48b799c64e561ad055b30b7805823e
Whitenessly <whitenessly@gmail.com>
2025-07-07 20:40:27 +0700

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-import { useState } from 'react'
 import './App.css'
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router';
+import Loading from './Components/Loading';
+import SignUp from './Components/SignUp';
+import Login from './Components/Login';
 
 function App() {
+  const apiKey = "6855203daa0c8c0805c3bd3d";
+  // const [users, setUsers] = useState([]);
+  React.useEffect(() => {
+    fetch(`https://mindx-mockup-server.vercel.app/api/resources/oke?apiKey=${apiKey}`)
+      // {
+      //   method: "GET",
+      //   headers: {
+      //     "apiKey": "6855203daa0c8c0805c3bd3d"
+      //   }
+      // })
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        console.log(data.data.data.users)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, []);
+  // console.log(users);
 
   return (
-    <>
-
-    </>
+    <div>
+      <Routes>
+        <Route path='' element={<Loading />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/Components/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Logo from '../assets/Logo.svg'
+import { Link } from 'react-router';
+
+const Login = () => {
+
+    const body ={
+        username: "Wsly",
+        email: "whitenessly@gmail.com",
+        password: "23212332"
+    }
+
+    return (
+        <div className='w-screen h-screen flex flex-col items-center justify-between py-4 px-8'>
+            <div className='w-full flex flex-col items-center gap-3'>
+                <img src={Logo} alt="Logo" />
+                <p className='font-bold text-2xl'>Create New Account</p>
+                <p className='text-[#A9A2A3] text-center'>
+                    Set up your username and password.
+                    <br />
+                    You can always change it later.
+                </p>
+                <form className='w-full flex flex-col gap-3'>
+                    <input type="text" className='border-[#A9A2A333] border-2 w-full px-5 py-3 rounded-lg focus:border-[#FF515A] ' placeholder='Username'/>
+                    <input type="email" className='border-[#A9A2A333] border-2 w-full px-5 py-3 rounded-lg focus:border-[#FF515A] ' placeholder='Email' />
+                    <input type="password" className='border-[#A9A2A333] border-2 w-full px-5 py-3 rounded-lg focus:border-[#FF515A] ' placeholder='Password' />
+                    <input type="password" className='border-[#A9A2A333] border-2 w-full px-5 py-3 rounded-lg focus:border-[#FF515A] ' placeholder='Confirm password' />
+                    <button className='w-full px-5 py-3 rounded-lg bg-[#FF515A] text-white'>Sign up</button>
+                </form>
+            </div>
+            <div>Already have an account? <Link to={'/login'} className='text-[#FF515A]'>Log in</Link></div>
+        </div>
+    )
+}
+
+export default Login
